22ba46c636cd43161bcf11bf6788cdd61a9167cf
Quach Cong Tuan <84640308+TunnieTek@users.noreply.github.com>
2023-02-17 05:07:12 +0700

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,180 @@
+document.getElementById("asm1-iot").addEventListener("click", function () {
+  var name = prompt("Enter Your Code");
+  if (name == "iot2022-tuanqc") {
+    alert("Xác Thực Thành Công!");
+    var link = document.createElement("a");
+    link.href = "Docs/ASM1-IOT.pdf";
+    link.download = "ASM1.pdf";
+    link.click();
+  } else {
+    alert("Xác Thực Thất Bại!");
+  }
+});
+document.getElementById("asm2-iot").addEventListener("click", function () {
+  var name = prompt("Enter Your Code");
+  if (name == "iot2022-tuanqc") {
+    alert("Xác Thực Thành Công!");
+    var link = document.createElement("a");
+    link.href = "Docs/ASM2-IOT.pdf";
+    link.download = "ASM2.pdf";
+    link.click();
+  } else {
+    alert("Xác Thực Thất Bại!");
+  }
+});
+
+document.getElementById("crp1").addEventListener("click", function () {
+  var name = prompt("Enter Your Code");
+  if (name == "tuanqc-crp-2022") {
+    alert("Xác Thực Thành Công!");
+    var link = document.createElement("a");
+    link.href = "Docs/CRP1.pdf";
+    link.download = "ASM1.pdf";
+    link.click();
+  } else {
+    alert("Xác Thực Thất Bại!");
+  }
+});
+document.getElementById("crp2").addEventListener("click", function () {
+  var name = prompt("Enter Your Code");
+  if (name == "tuanqc-crp-2022") {
+    alert("Xác Thực Thành Công!");
+    var link = document.createElement("a");
+    link.href = "Docs/CRP2.pdf";
+    link.download = "ASM2.pdf";
+    link.click();
+  } else {
+    alert("Xác Thực Thất Bại!");
+  }
+});
+
+document.getElementById("sc1").addEventListener("click", function () {
+  var name = prompt("Enter Your Code");
+  if (name == "tuanqc2022-sc") {
+    alert("Xác Thực Thành Công!");
+    var link = document.createElement("a");
+    link.href = "Docs/Security1.pdf";
+    link.download = "SC-ASM1.pdf";
+    link.click();
+  } else {
+    alert("Xác Thực Thất Bại!");
+  }
+});
+document.getElementById("sc2").addEventListener("click", function () {
+  var name = prompt("Enter Your Code");
+  if (name == "tuanqc2022-sc") {
+    alert("Xác Thực Thành Công!");
+    var link = document.createElement("a");
+    link.href = "Docs/Security2.pdf";
+    link.download = "SC-ASM2.pdf";
+    link.click();
+  } else {
+    alert("Xác Thực Thất Bại!");
+  }
+});
+
+document.getElementById("ad1").addEventListener("click", function () {
+  var name = prompt("Enter Your Code");
+  if (name == "qcongtuan2001") {
+    alert("Xác Thực Thành Công!");
+    var link = document.createElement("a");
+    link.href = "Docs/AD1.pdf";
+    link.download = "ASM1.pdf";
+    link.click();
+  } else {
+    alert("Xác Thực Thất Bại!");
+  }
+});
+document.getElementById("ad2").addEventListener("click", function () {
+  var name = prompt("Enter Your Code");
+  if (name == "qcongtuan2001") {
+    alert("Xác Thực Thành Công!");
+    var link = document.createElement("a");
+    link.href = "Docs/AD2.pdf";
+    link.download = "ASM2.pdf";
+    link.click();
+  } else {
+    alert("Xác Thực Thất Bại!");
+  }
+});
+
+document.getElementById("fpt-training").addEventListener("click", function () {
+  //Tạo alert nhập thông tin
+  var name = prompt("Enter Your Code");
+  //Kiểm tra thông tin
+  if (name == "tunnie@2001") {
+    alert("Xác Thực Thành Công!");
+    //Create download link
+    var link = document.createElement("a");
+    link.href =
+      "https://drive.google.com/uc?export=download&id=19R6uuqheqW986oqkuSvCWfYeMhCxJa4F";
+    link.download = "FPTTraining.zip";
+    link.click();
+  } else {
+    alert("Xác Thực Thất Bại!");
+  }
+});
+document.getElementById("bevis").addEventListener("click", function () {
+  //Tạo alert nhập thông tin
+  var name = prompt("Enter Your Code");
+  //Kiểm tra thông tin
+  if (name == "tunnie@2001") {
+    alert("Xác Thực Thành Công!");
+    //Create download link
+    var link = document.createElement("a");
+    link.href =
+      "https://drive.google.com/uc?export=download&id=1xoJ7ZrY2K92n2k1zFe3lIKzgnM4mzFqW";
+    link.download = "BevisSneaker.zip";
+    link.click();
+  } else {
+    alert("Xác Thực Thất Bại!");
+  }
+});
+document.getElementById("bedating").addEventListener("click", function () {
+  //Tạo alert nhập thông tin
+  var name = prompt("Enter Your Code");
+  //Kiểm tra thông tin
+  if (name == "tunnietek") {
+    alert("Xác Thực Thành Công!");
+  } else {
+    alert("Xác Thực Thất Bại!");
+  }
+});
+
+document.getElementById("bi1").addEventListener("click", function () {
+  var name = prompt("Enter Your Code");
+  if (name == "bi2022-tuanqc") {
+    alert("Xác Thực Thành Công!");
+    var link = document.createElement("a");
+    link.href = "Docs/BI1.pdf";
+    link.download = "ASM1.pdf";
+    link.click();
+  } else {
+    alert("Xác Thực Thất Bại!");
+  }
+});
+document.getElementById("bi2").addEventListener("click", function () {
+  var name = prompt("Enter Your Code");
+  if (name == "bi2022-tuanqc") {
+    alert("Xác Thực Thành Công!");
+    var link = document.createElement("a");
+    link.href = "Docs/BI2.pdf";
+    link.download = "ASM2.pdf";
+    link.click();
+  } else {
+    alert("Xác Thực Thất Bại!");
+  }
+});
+
+document.getElementById("bi3").addEventListener("click", function () {
+  var name = prompt("Enter Your Code");
+  if (name == "bi2022-tuanqc") {
+    alert("Xác Thực Thành Công!");
+    var link = document.createElement("a");
+    link.href = "Docs/BI3.pdf";
+    link.download = "ASM1.pdf";
+    link.click();
+  } else {
+    alert("Xác Thực Thất Bại!");
+  }
+});
